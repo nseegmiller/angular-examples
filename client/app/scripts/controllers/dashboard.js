617f34f0c62f5253938b914090b568028e95217a
Nick Seegmiller <nseegmiller@vivint.com>
2015-03-02 16:27:09 -0700

    Added a handful of useful new things. Incremental commit.

--- client/app/scripts/controllers/dashboard.js
+++ client/app/scripts/controllers/dashboard.js
@@ -1,7 +1,36 @@
-angular.module('angular-examples').controller('DashboardCtrl', ['$scope', '$location',
-    function($scope, $location) {
+angular.module('angular-examples').controller('DashboardCtrl', ['$scope', '$location', '$rootScope',
+    function ($scope, $location, $rootScope) {
+        // Object to avoid shadowing/scoping issues
+        $scope.sortDir = {
+            reverse: false
+        };
+
+        $scope.list = [
+            {action: function() { toggleVariable('ngif') }, label: 'ng-if', sort: 1},
+            {action: function() { toggleVariable('nginclude') }, label: 'ng-include', sort: 2},
+            {action: function() { toggleVariable('keyframes') }, label: 'keyframes', sort: 3},
+            {action: function() { $scope.go('/other-page', 'opacity-fade') }, label: 'change views - broken', sort: 4},
+            {action: function() { $scope.go('/other-page', 'opacity-fade-absolute') }, label: 'change views - absolute', sort: 5},
+            {action: function() { toggleRootVariable('compliment') }, label: 'ng-class', sort: 6},
+            {action: function() { toggleVariable('ngshow') }, label: 'ng-show', sort: 7},
+            {action: function() { toggleVariable('multipart') }, label: 'multipart', sort: 9},
+            {action: function() { toggleVariable('lotsofprops') }, label: 'lots of props', sort: 10}
+        ];
+
         $scope.ngif = false;
-        $scope.toggleVariable = function(variable) {
+
+        // Set an initial transition class. I'd normally do this in some sort of parent controller
+        $rootScope.transitionClass = 'opacity-fade';
+
+        // A very handy navigation function that lets you use unique transitions for each navigation
+        $scope.go = function (page, transitionClass) {
+            if (transitionClass !== undefined) {
+                $rootScope.transitionClass = transitionClass;
+            }
+            $location.path(page);
+        };
+
+        var toggleVariable = function (variable) {
             if (variable === 'nginclude') {
                 if (!$scope[variable]) {
                     $scope[variable] = 'views/sub-doge.html';
@@ -13,6 +42,10 @@ angular.module('angular-examples').controller('DashboardCtrl', ['$scope', '$loca
             else {
                 $scope[variable] = !$scope[variable];
             }
+        };
+
+        var toggleRootVariable = function (variable) {
+            $rootScope[variable] = !$rootScope[variable];
         };
 
         $scope.$location = $location;
